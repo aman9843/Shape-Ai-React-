ade4ce77965fac7e995bf6c235c2c37ae47a0081
aman9843 <amanprasad9843@gmail.com>
2021-07-29 14:58:58 +0530

    Intial commit

---
+++ src/components/Boot.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1>React js Bootcamp</h1>
+      <p>This was an amazing Bootcamp I learned a Lot!</p>
+    </div>
+  );
+}
+
+export default Boot;
